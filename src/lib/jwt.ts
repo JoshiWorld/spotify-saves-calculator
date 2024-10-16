e6e25c65e727829b644ef69aecc2bc051a19c7ab
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-10-16 18:06:58 +0200

    fix: build errors

--- src/lib/jwt.ts
+++
@@ -1,9 +0,0 @@
-import { env } from "@/env";
-import jwt from "jsonwebtoken";
-
-export function generateJWT(userId: string, email: string) {
-  const payload = { userId, email };
-  const secret = env.JWT_SECRET;
-
-  return jwt.sign(payload, secret, { expiresIn: "30d" });
-};
