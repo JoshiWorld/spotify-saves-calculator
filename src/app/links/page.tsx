fffb8b04fb75378a4381d4b2230c62d7251df80a
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-10-24 23:14:27 +0200

    feature: linkstats can be seen in links overview

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -3,6 +3,7 @@ import { Links } from "../_components/links";
 
 export default function Page() {
     void api.link.getAll.prefetch();
+    void api.linkstats.getAll.prefetch();
 
     return <Links />;
 }
