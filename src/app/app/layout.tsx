8be889218b645d53dd9149121d927ef933c0968b
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-10-30 15:00:57 +0100

    fix: user fetch on user not exist

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,11 +1,14 @@
 import { getServerAuthSession } from "@/server/auth";
 import { NavbarLoggedIn } from "@/app/_components/landing/navbar";
 import { api } from "@/trpc/server";
+import { redirect } from "next/navigation";
 
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const session = await getServerAuthSession();
+  if (!session?.user) return redirect("/login");
+  
   await api.user.get.prefetch();
 
   return (
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,11 +1,6 @@
-import { getServerAuthSession } from "@/server/auth";
 import HomePage from "@/app/_components/home";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await getServerAuthSession();
-  if(!session?.user) return redirect('/login');
-
   return (
     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
       <HomePage />
