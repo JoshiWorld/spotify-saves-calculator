4365b2d36731ed018f218c5ca9cfb46d9951d575
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-12-05 15:18:50 +0100

    update: lowered cookie banner color for better barrier

--- src/app/_components/links/cookie-banner.tsx
+++ src/app/_components/links/cookie-banner.tsx
@@ -8,7 +8,6 @@ export const CookieBanner: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    // Überprüfe, ob der Cookie gesetzt ist
     const cookiePreference = getCookie(COOKIE_NAME);
     if (!cookiePreference) {
       setIsVisible(true);
@@ -46,7 +45,7 @@ export const CookieBanner: React.FC = () => {
     <div className="fixed bottom-0 z-50 flex w-full flex-col items-center justify-center bg-background p-4 text-white shadow-lg sm:flex-row">
       <div className="flex flex-col gap-2">
         <button
-          className="rounded bg-green-600 px-4 py-2 text-sm text-white transition-all hover:bg-green-700 sm:text-base"
+          className="rounded bg-green-700 px-4 py-2 text-sm text-white transition-all hover:bg-green-800 sm:text-base"
           onClick={acceptCookies}
         >
           Annehmen
