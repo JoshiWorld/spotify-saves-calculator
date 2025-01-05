483d80861563aa09e9d85ff844e2e6da1d1fd0d8
Joshua Stieber <joshua.stieber.1903@gmail.com>
2025-01-05 22:02:23 +0100

    fix: link zugriff fix

--- src/app/app/links/page.tsx
+++ src/app/app/links/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
   const user = await api.user.get();
-  if (!user?.admin || !user.package) return redirect("/app/abo");
+  if (!user?.admin && !user?.package) return redirect("/app/abo");
 
   await api.link.getAllView.prefetch();
   await api.linkstats.getVisits.prefetch();
