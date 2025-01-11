68a3a8baf7519ae17b48f7f62bf3434687ae3578
Joshua Stieber <joshua.stieber.1903@gmail.com>
2025-01-12 00:02:09 +0100

    update: new design for user settings page

--- src/app/_components/app/user-settings.tsx
+++ src/app/_components/app/user-settings.tsx
@@ -23,12 +23,15 @@ import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
 import { useToast } from "@/hooks/use-toast";
 import { signOut } from "next-auth/react";
+import { LogOutIcon } from "lucide-react";
+import { IconBrandMeta } from "@tabler/icons-react";
 
 const formSchema = z.object({
   name: z.string().min(2).max(50),
   // image: z.string().min(2),
   goodCPS: z.string(),
   midCPS: z.string(),
+  email: z.string().email(),
 });
 
 export function UserSettings() {
@@ -36,6 +39,17 @@ export function UserSettings() {
   const router = useRouter();
   const utils = api.useUtils();
   const [user] = api.user.getSettings.useSuspenseQuery();
+
+  const removeMetaAccess = api.user.removeMetaAccess.useMutation({
+    onSuccess: async () => {
+      await utils.user.invalidate();
+      toast({
+        variant: "default",
+        title: "Dein Meta-Zugriff wurde entfernt.",
+      });
+      router.refresh();
+    },
+  });
 
   const updateUser = api.user.update.useMutation({
     onSuccess: async () => {
@@ -71,6 +85,7 @@ export function UserSettings() {
       // image: user!.image,
       goodCPS: String(user!.goodCPS),
       midCPS: String(user!.midCPS),
+      email: user!.email!
     },
   });
 
@@ -83,10 +98,35 @@ export function UserSettings() {
     });
   }
 
+  function metaButton() {
+    if(user!.metaAccessToken) {
+      removeMetaAccess.mutate();
+    } else {
+      router.push("/api/meta/login");
+    }
+  }
+
   return (
     <Form {...form}>
+      <div className="flex items-center justify-between gap-10">
+        <LogOutIcon
+          className="cursor-pointer text-red-500 transition hover:text-red-700"
+          onClick={() => signOut()}
+        />
+        <div className="relative">
+          <IconBrandMeta className="cursor-pointer text-blue-500 transition hover:text-blue-700" onClick={() => metaButton()} />
+          {user!.metaAccessToken ? (
+            <span className="absolute -right-2 -top-2 flex h-4 w-4 items-center justify-center rounded-full bg-green-500 text-xs font-bold text-white">
+              ✓
+            </span>
+          ) : (
+            <span className="absolute -right-2 -top-2 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs font-bold text-white">
+              ✕
+            </span>
+          )}
+        </div>
+      </div>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <Button onClick={() => signOut()}>Ausloggen</Button>
         <FormField
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
           control={form.control}
@@ -98,38 +138,7 @@ export function UserSettings() {
                 <Input type="text" placeholder="Max Mustermann" {...field} />
               </FormControl>
               <FormDescription>
-                Dieser Name wird im Forum angezeigt.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {/* <FormField
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-          control={form.control}
-          name="image"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Profilbild</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="Link zu Bild" {...field} />
-              </FormControl>
-              <FormDescription>Dein Profilbild.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        /> */}
-        <FormField
-          control={form.control}
-          name="goodCPS"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Good-CPS</FormLabel>
-              <FormControl>
-                <Input type="number" placeholder="GoodCPS" {...field} />
-              </FormControl>
-              <FormDescription>
-                Alles was unter diesem Wert ist, wird als grün gekennzeichnet
+                Dieser Name wird im Forum angezeigt
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -137,31 +146,74 @@ export function UserSettings() {
         />
         <FormField
           control={form.control}
-          name="midCPS"
+          name="email"
+          disabled
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Mid-CPS</FormLabel>
+              <FormLabel>E-Mail</FormLabel>
               <FormControl>
-                <Input type="number" placeholder="MidCPS" {...field} />
+                <Input
+                  type="email"
+                  placeholder="max.mustermann@email.de"
+                  {...field}
+                />
               </FormControl>
-              <FormDescription>
-                Alles was unter diesem Wert ist, wird als gelb gekennzeichnet.
-                Alles ab diesem Wert und darüber wird als Rot gekennzeichnet
-              </FormDescription>
+              <FormDescription>Deine E-Mail für den Login</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+        <div className="flex justify-between gap-5">
+          <FormField
+            control={form.control}
+            name="goodCPS"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Good-CPS</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="GoodCPS" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Alles was unter diesem Wert ist, wird als grün gekennzeichnet
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="midCPS"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Mid-CPS</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="MidCPS" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Alles was unter diesem Wert ist, wird als gelb gekennzeichnet.
+                  Alles ab diesem Wert und darüber wird als Rot gekennzeichnet
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
         <div className="flex justify-center gap-5">
           <Button
             disabled={deleteUser.isPending}
+            className="flex-1"
             type="button"
             variant={"destructive"}
-            onClick={() => deleteUser.mutate()}
+            // onClick={() => deleteUser.mutate()}
+            onClick={() => console.log("Account löschen")}
           >
             {deleteUser.isPending ? "Wird gelöscht.." : "Account löschen"}
           </Button>
-          <Button type="submit" disabled={updateUser.isPending}>
+          <Button
+            type="submit"
+            disabled={updateUser.isPending}
+            className="flex-1"
+          >
             {updateUser.isPending ? "Wird gespeichert.." : "Speichern"}
           </Button>
         </div>
