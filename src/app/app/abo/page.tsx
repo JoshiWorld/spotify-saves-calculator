10ad5699fd1e7be6981e5c976695c57f754b23bf
Joshua Stieber <joshua.stieber.1903@gmail.com>
2025-01-13 13:10:30 +0100

    fix: prefetching errors

--- src/app/app/abo/page.tsx
+++ src/app/app/abo/page.tsx
@@ -1,12 +1,18 @@
 import { PricingLoggedIn } from "@/app/_components/app/pricing";
-import { api } from "@/trpc/server";
+// import { api } from "@/trpc/server";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Upgrade dein Abo | SmartSavvy",
+  description: "Hier kannst du dein Abo hochstufen oder ändern.",
+};
 
 export default async function Page() {
-    await api.product.getAll.prefetch();
+  // await api.product.getAll.prefetch();
 
-    return (
-      <div>
-        <PricingLoggedIn />
-      </div>
-    );
+  return (
+    <div>
+      <PricingLoggedIn />
+    </div>
+  );
 }
--- src/app/app/projects/page.tsx
+++ src/app/app/projects/page.tsx
@@ -1,9 +1,15 @@
 import { Projects } from "@/app/_components/app/projects";
-import { api } from "@/trpc/server";
+// import { api } from "@/trpc/server";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Spotify Saves Calculator | SmartSavvy",
+  description: "Deine Projekte aus dem Spotify Saves Calculator",
+};
 
 export default async function Home() {
-  await api.project.getAll.prefetch();
-  await api.meta.getMetaAccounts.prefetch();
+  // await api.project.getAll.prefetch();
+  // await api.meta.getMetaAccounts.prefetch();
 
   return <Projects />;
 }
