bd2d8ed26b3efcb4c84ecf5fd96e7c0ac1e37dce
Joshua Stieber <joshua.stieber.1903@gmail.com>
2025-01-20 10:17:56 +0100

    fix: digistore types

--- src/app/api/digistore/webhook/route.ts
+++ src/app/api/digistore/webhook/route.ts
@@ -1,3 +1,4 @@
+import { type DigistoreIPN } from "@/lib/digistore";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -6,11 +7,17 @@ export async function POST(req: Request) {
     console.log("Raw body:", body);
 
     // Form-Daten parsen
-    const parsedBody = Object.fromEntries(new URLSearchParams(body));
+    const parsedBody = Object.fromEntries(
+      new URLSearchParams(body),
+    ) as DigistoreIPN;
     console.log("Parsed body:", parsedBody);
 
+    // Beispiel: Zugriff auf die Felder
+    const email = parsedBody.email ?? "Nicht angegeben";
+    const productName = parsedBody.product_name ?? "Nicht angegeben";
+
     return NextResponse.json(
-      { message: "IPN erfolgreich verarbeitet", data: parsedBody },
+      { message: "IPN erfolgreich verarbeitet", data: { email, productName } },
       { status: 200 },
     );
   } catch (error) {
