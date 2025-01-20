9007ad2a95254f915bd78687d154e88e92f0453f
Joshua Stieber <joshua.stieber.1903@gmail.com>
2025-01-20 10:11:44 +0100

    fix: digistore

--- src/app/api/digistore/webhook/route.ts
+++ src/app/api/digistore/webhook/route.ts
@@ -1,54 +1,10 @@
 import { NextResponse } from "next/server";
-import { api } from "@/trpc/server";
-import { type Copecart, verifyCopeCartSignature } from "@/lib/copecart";
-import { LogType } from "@prisma/client";
 
 export async function POST(req: Request) {
   try {
-    // const body = await req.text();
+    const body = await req.text();
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const body = await req.json();
-    // const signature = req.headers.get("x-copecart-signature");
-
-    // if (!verifyCopeCartSignature(body, signature)) {
-    //   return NextResponse.json(
-    //     { error: "Ungültige Signatur" },
-    //     { status: 401 },
-    //   );
-    // }
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    await api.log.create({
-      logtype: LogType.INFO,
-      message: JSON.stringify(body),
-    });
-
-    return NextResponse.json(
-      { message: "IPN erfolgreich verarbeitet" },
-      { status: 200 },
-    );
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    // const data: Copecart = JSON.parse(body);
-
-    // Prüfen, welches Event gesendet wurde
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    // const { event_type, ...payload } = data;
-
-    // const userEmail = payload.buyer_email;
-    // const productName = payload.product_internal_name;
-
-    // switch (event_type) {
-    //   case "payment.made":
-    //   case "payment.trial":
-    //     await updateUserSubscription(userEmail, productName, body, signature);
-    //     break;
-    //   case "payment.failed":
-    //   case "payment.refunded":
-    //   case "payment.charged_back":
-    //     await cancelUserSubscription(userEmail, body, signature);
-    //     break;
-    // }
+    // const body = await req.json();
 
     return NextResponse.json(
       { message: "IPN erfolgreich verarbeitet" },
@@ -61,21 +17,4 @@ export async function POST(req: Request) {
       { status: 500 },
     );
   }
-}
-
-async function updateUserSubscription(
-  email: string,
-  productName: string,
-  body: string,
-  signature: string | null,
-) {
-  await api.user.updateSubscription({ email, productName, body, signature });
-}
-
-async function cancelUserSubscription(
-  email: string,
-  body: string,
-  signature: string | null,
-) {
-  await api.user.updateSubscription({ email, body, signature });
 }
