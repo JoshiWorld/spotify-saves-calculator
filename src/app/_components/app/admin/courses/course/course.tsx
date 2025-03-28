b1d48c18da6eff52ce33265ee0961e9cc8404860
Joshua Stieber <joshua.stieber.1903@gmail.com>
2025-03-28 20:16:40 +0100

    update: added sections to course view #222

--- src/app/_components/app/admin/courses/course/course.tsx
+++ src/app/_components/app/admin/courses/course/course.tsx
@@ -1,17 +1,40 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { LoadingSkeleton } from "@/components/ui/loading";
+import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/hooks/use-toast";
 import { api } from "@/trpc/react";
 import { UploadIcon } from "lucide-react";
 import * as NextImage from "next/image";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { CreateSection } from "../sections/create-section";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+type MinSection = {
+  id: string;
+  title: string;
+  videos: {
+    id: string;
+    description: string | null;
+    title: string;
+    thumbnail: string;
+    videoLink: string;
+    usersWatched: {
+      id: string;
+    }[];
+  }[];
+};
 
 export function CourseView({ id }: { id: string }) {
   const utils = api.useUtils();
   const { toast } = useToast();
 
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [description, setDescription] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
 
   const { data: course, isLoading: isLoadingCourse } =
     api.course.getCourse.useQuery({ id });
@@ -24,6 +47,13 @@ export function CourseView({ id }: { id: string }) {
       });
     },
   });
+
+  useEffect(() => {
+    if (course) {
+      setDescription(course.description ?? "");
+      setTitle(course.title);
+    }
+  }, [course]);
 
   if (isLoadingCourse || !course) {
     return <LoadingSkeleton />;
@@ -91,7 +121,7 @@ export function CourseView({ id }: { id: string }) {
   };
 
   return (
-    <div className="flex w-full flex-col items-center gap-3">
+    <div className="flex w-full flex-col items-center gap-5">
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         {course.title}
       </h2>
@@ -106,7 +136,6 @@ export function CourseView({ id }: { id: string }) {
           }}
           className="rounded-md"
         />
-
         <div className="avatar-overlay" onClick={handleUploadClick}>
           <UploadIcon className="h-4 w-4" />
         </div>
@@ -118,6 +147,77 @@ export function CourseView({ id }: { id: string }) {
           ref={fileInputRef}
         />
       </div>
+
+      <div className="flex w-1/2 items-start justify-start gap-4">
+        <Label htmlFor="title" className="w-32 text-left">
+          Titel:
+        </Label>
+        <Input
+          id="title"
+          value={title}
+          disabled
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full" // Input füllt den restlichen Platz aus
+        />
+      </div>
+
+      <div className="flex w-1/2 items-start justify-start gap-4">
+        <Label htmlFor="description" className="w-32 text-left">
+          Beschreibung:
+        </Label>
+        <Textarea
+          id="description"
+          disabled
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="w-full" // Textarea füllt den restlichen Platz aus
+        />
+      </div>
+
+      <Button>Eingaben speichern</Button>
+
+      <SectionsTable sections={course.sections} />
+
+      <CreateSection courseId={course.id} />
+    </div>
+  );
+}
+
+function SectionsTable({ sections }: { sections: MinSection[] }) {
+  if (sections.length === 0) {
+    return <p>In diesem Kurs gibt es noch keine Sections.</p>;
+  }
+
+  return (
+    <div className="mt-5 flex w-1/2 flex-col items-center gap-5">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Sections
+      </h3>
+      {sections.map((section, idx) => (
+        <div key={section.id} className="w-full">
+          <Card className="w-full">
+            <CardContent className="py-3">
+              <div className="flex justify-between">
+                <div className="flex flex-col">
+                  <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                    {idx + 1}. {section.title}
+                  </h4>
+                  <p className="text-sm text-muted-foreground">
+                    {section.videos.length} Videos
+                  </p>
+                </div>
+
+                <div className="flex w-1/2 items-center justify-end gap-3">
+                  <Button variant="subtle" className="w-1/2">
+                    Bearbeiten
+                  </Button>
+                  <Button variant="destructive">Löschen</Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      ))}
     </div>
   );
 }
